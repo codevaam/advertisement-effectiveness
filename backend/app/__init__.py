07e94de05302878face8637a6e698d5aaad39ec4
devamtrivedi <devamtrivedi71@gmail.com>
2021-04-01 12:42:33 +0530

    fix: flask env

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 import random
 import json
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 from app import views
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,17 +1,35 @@
 from app import app
 import numpy as np
 import cv2
+from flask import render_template, Response
+
+camera = cv2.VideoCapture(0)
 
 @app.route("/")
 def index():
-    while(True):
-        ret, fram = cap.read()
+    return "hello"
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def gen_frames():
+    while(True):
+        sucess, frame = camera.read()
+        if not sucess:
+            break
+
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
