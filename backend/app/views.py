17ef884af8190de951daac00421ae954a645467e
devamtrivedi <devamtrivedi71@gmail.com>
2021-04-27 22:09:52 +0530

    feat: add eye tracking using dlib

--- backend/app/views.py
+++ backend/app/views.py
@@ -2,24 +2,46 @@ from app import app
 import numpy as np
 import cv2
 from flask import render_template, Response
+import dlib
 
-camera = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(cv2.CAP_V4L2)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 @app.route("/")
 def index():
     return "hello"
 
-def gen_frames():
-    while(True):
-        sucess, frame = camera.read()
-        if not sucess:
-            break
+def midpoint(p1 ,p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+def gen_frames():
+    while True:
+        _, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = detector(gray)
+        for face in faces:
+            #x, y = face.left(), face.top()
+            #x1, y1 = face.right(), face.bottom()
+            #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+
+            landmarks = predictor(gray, face)
+            left_point = (landmarks.part(36).x, landmarks.part(36).y)
+            right_point = (landmarks.part(39).x, landmarks.part(39).y)
+            center_top = midpoint(landmarks.part(37), landmarks.part(38))
+            center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+
+            hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+            ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+            cv2.imshow("Frame", frame)
+
+        key = cv2.waitKey(1)
+        if key==27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 @app.route("/home")
@@ -29,7 +51,3 @@ def home():
 @app.route("/video_feed")
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
